f64da66660bba9b46561939fa853f441c2fecb66
Kiryl Henets <kirill.genets2002@mail.ru>
2023-01-06 09:31:31 +0300

    Create form without styles

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,145 @@
+import { useCallback, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import Validator from 'simple-react-validator';
+
+const FieldName = {
+  FirstName: 'firstName',
+  LastName: 'lastName',
+  Email: 'email',
+  BirthDate: 'birthDate',
+};
+
+const VALIDATION_RULES = {
+  [FieldName.FirstName]: 'required|alpha_space',
+  [FieldName.LastName]: 'required|alpha_space',
+  [FieldName.Email]: 'required|email',
+  [FieldName.BirthDate]: 'required|regex:[0-9]{4}-[0-9]{2}-[0-9]{2}',
+};
+
+const DEFAULT_FORM_DATA = {
+  [FieldName.FirstName]: '',
+  [FieldName.LastName]: '',
+  [FieldName.Email]: '',
+  [FieldName.BirthDate]: '',
+};
+
+const Form = ({ onSubmit }) => {
+  const validatorRef = useRef(
+    new Validator({
+      element: (message) => <p className="form__error">{message}</p>,
+    }),
+  );
+  const [formData, setFormData] = useState(DEFAULT_FORM_DATA);
+  const [, setIsError] = useState(false);
+
+  const handleFieldChange = useCallback(
+    (name) => (evt) => {
+      setFormData((prevFormData) => ({ ...prevFormData, [name]: evt.target.value }));
+    },
+    [],
+  );
+
+  const handleSubmit = useCallback(
+    (evt) => {
+      evt.preventDefault();
+      if (validatorRef.current.allValid()) {
+        onSubmit(formData);
+        setFormData(DEFAULT_FORM_DATA);
+      } else {
+        validatorRef.current.showMessages();
+        setIsError(true);
+      }
+    },
+    [formData, onSubmit],
+  );
+
+  return (
+    <form action="#" className="form" onSubmit={handleSubmit}>
+      <div className="form__fieldset">
+        <label htmlFor={FieldName.FirstName} className="form__label">
+          First name:
+        </label>
+        <input
+          type="text"
+          name={FieldName.FirstName}
+          id={FieldName.FirstName}
+          value={formData[FieldName.FirstName]}
+          className="form__input"
+          placeholder="Please, type your first name..."
+          onChange={handleFieldChange(FieldName.FirstName)}
+        />
+        {validatorRef.current.message(
+          FieldName.FirstName,
+          formData[FieldName.FirstName],
+          VALIDATION_RULES[FieldName.FirstName],
+        )}
+      </div>
+      <div className="form__fieldset">
+        <label htmlFor={FieldName.LastName} className="form__label">
+          Last name:
+        </label>
+        <input
+          type="text"
+          name={FieldName.LastName}
+          id={FieldName.LastName}
+          value={formData[FieldName.LastName]}
+          className="form__input"
+          placeholder="Please, type your last name..."
+          onChange={handleFieldChange(FieldName.LastName)}
+        />
+        {validatorRef.current.message(
+          FieldName.LastName,
+          formData[FieldName.LastName],
+          VALIDATION_RULES[FieldName.LastName],
+        )}
+      </div>
+      <div className="form__fieldset">
+        <label htmlFor={FieldName.Email} className="form__label">
+          Email:
+        </label>
+        <input
+          type="email"
+          name={FieldName.Email}
+          id={FieldName.Email}
+          value={formData[FieldName.Email]}
+          className="form__input"
+          placeholder="Please, type your email..."
+          onChange={handleFieldChange(FieldName.Email)}
+        />
+        {validatorRef.current.message(
+          FieldName.Email,
+          formData[FieldName.Email],
+          VALIDATION_RULES[FieldName.Email],
+        )}
+      </div>
+      <div className="form__fieldset">
+        <label htmlFor={FieldName.BirthDate} className="form__label">
+          Date of birth:
+        </label>
+        <input
+          type="date"
+          name={FieldName.BirthDate}
+          id={FieldName.BirthDate}
+          value={formData[FieldName.BirthDate]}
+          className="form__input"
+          placeholder="Please, type your date of birth..."
+          onChange={handleFieldChange(FieldName.BirthDate)}
+        />
+        {validatorRef.current.message(
+          FieldName.BirthDate,
+          formData[FieldName.BirthDate],
+          VALIDATION_RULES[FieldName.BirthDate],
+        )}
+      </div>
+      <button type="submit" className="form__submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Form;
